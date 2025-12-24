11574d703d975de434945de5714c32b9544b89ad
lingyuze1996 <62694056+lingyuze1996@users.noreply.github.com>
2025-12-24 14:50:11 +1100

    Add multiple files upload support

--- src/utils/assetsUrlMapper.ts
+++ src/utils/assetsUrlMapper.ts
@@ -1,4 +1,10 @@
 export const getThumbnailUrl = (id: string) => {
+  return `https://album.storage.daydayhealth.click/cdn-cgi/image/width=75/${encodeURIComponent(
+    id
+  )}/original`;
+};
+
+export const getOriginalUrl = (id: string) => {
   return `https://album.storage.daydayhealth.click/${encodeURIComponent(
     id
   )}/original`;
